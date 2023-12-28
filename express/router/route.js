b7d52475646b0f332f9d8ae90d29bdf63b0bc213
sidhart25 <navneetnishchal1420@gmail.com>
2023-12-28 18:20:37 +0530

    route.js of express source code is completed

--- express/router/route.js
+++ express/router/route.js
@@ -27,22 +27,22 @@ function Route(path) {
 //adding prototype simply means that this _handle_method is available for Route instances
 //also so each instance can call it
 Route.prototype._handles_method = function _handles_method(method) {
-    //this function simply return a boolean value,it returns true when the method 
-    //if the current route instance is configured to handle the current
-    //http method
-    //if the route instance method is found in methods array,then it returns true
-    //otherwise it return false
+  //this function simply return a boolean value,it returns true when the method 
+  //of the current route instance is configured to handle the current
+  //http method
+  //if the route instance method is found in methods array,then it returns true
+  //otherwise it return false
 
-    /*
-    app.all('/example', (req, res) => {
-        res.send('This route handles all HTTP methods.');
-      });
-      when a route is called using all method,this means that this route should get
-      handles for every method
-      */
-     //when we call app.all() express internally sets the _all flag of methods object 
-     // to true
-      
+  /*
+  app.all('/example', (req, res) => {
+      res.send('This route handles all HTTP methods.');
+    });
+    when a route is called using all method,this means that this route should get
+    handles for every method
+    */
+  //when we call app.all() express internally sets the _all flag of methods object 
+  // to true
+
   if (this.methods._all) {
     return true;
   }
@@ -53,7 +53,7 @@ Route.prototype._handles_method = function _handles_method(method) {
     : method
 
 
-    //head is considered to be a get method in express
+  //head is considered to be a get method in express
   if (name === 'head' && !this.methods['head']) {
     name = 'get';
   }
@@ -69,10 +69,20 @@ Route.prototype._handles_method = function _handles_method(method) {
 //the route can handle
 Route.prototype._options = function _options() {
   var methods = Object.keys(this.methods);
+  //if this.methods looks like { get: true, post: true }, then after this line, methods would be ['get', 'post'].
 
   // append automatic head
   if (this.methods.get && !this.methods.head) {
     methods.push('head');
+    /**
+     *  in HTTP, the HEAD method is similar to the GET method, but it requests the headers of the resource, not the actual resource.
+     *  However, if a route supports GET requests, it often makes sense to automatically support HEAD requests as well. 
+     * This is because the response to a HEAD request should be identical to the response to a GET request, but without the message body.
+
+        So, by appending 'head' to the list of methods, the route is signaling that it can handle HEAD requests in addition
+         to the methods explicitly defined on the route. This is a convenience in Express, allowing developers to handle HEAD
+          requests without explicitly defining a head route handler when a get handler is defined
+     */
   }
 
   for (var i = 0; i < methods.length; i++) {
@@ -85,58 +95,100 @@ Route.prototype._options = function _options() {
 
 
 
+
+
+
+
+
+//The purpose of this function is to iterate through the middleware and route handlers associated
+// with a route and execute them in the order they are defined.
 Route.prototype.dispatch = function dispatch(req, res, done) {
-  var idx = 0;
+  var idx = 0;//It maintains an index (idx) to keep track of the current layer being processed in the stack.
   var stack = this.stack;
   var sync = 0
+  //The sync variable in the dispatch function is used to prevent potential infinite loops in the case where next
+  // is called synchronously too many times.
 
   if (stack.length === 0) {
-    return done();
+    return done();//it means there are no middleware or route handlers for this route 
+    //so return the done function which is like completion function
   }
+
+  // getting the method from the req object and converting it into lowercase
   var method = typeof req.method === 'string'
     ? req.method.toLowerCase()
     : req.method
 
   if (method === 'head' && !this.methods['head']) {
-    method = 'get';
+    method = 'get';//head is treated as get request
   }
 
-  req.route = this;
+  req.route = this;//now req.route will be able to get the current instance of Route class
 
-  next();
+  next();//calling next
 
-  function next(err) {
+  function next(err) {//err is optional
     // signal to exit route
     if (err && err === 'route') {
+      //if error is equal to route,then it means that express app wanted to end this route means there 
+      // is no need to handle other middlewares of this route.we just want to exit
       return done();
     }
 
     // signal to exit router
     if (err && err === 'router') {
       return done(err)
+      //very similar to previous if statement
     }
 
     // max sync stack
     if (++sync > 100) {
+      //sync is increased every time next is called so it should not exceed 100
+      /**
+       * function synchronousMiddleware(req, res, next) {
+              let count = 0;
+
+              function recursiveNext() {
+                    if (count < 150) {
+                        count++;
+                        next();
+              recursiveNext();
+              }
+         }
+
+           recursiveNext();
+}
+
+         // Route with the synchronous middleware
+         app.get('/example', synchronousMiddleware, (req, res) => {
+          res.send('Hello, Express!');
+         });
+
+         so here we can see that we are calling next in a middleware 100 times so this is not feasible
+       */
       return setImmediate(next, err)
     }
 
-    var layer = stack[idx++]
+    var layer = stack[idx++]// middleware to be executed
 
     // end of layers
     if (!layer) {
       return done(err)
+      // no remaining layer
     }
 
     if (layer.method && layer.method !== method) {
+      //if requested method is not equal to the method in this layer then error
       next(err)
     } else if (err) {
       layer.handle_error(err, req, res, next);
+      //if error is provided 
     } else {
+      //handle the request
       layer.handle_request(req, res, next);
     }
 
-    sync = 0
+    sync = 0//now again set this to 0 
   }
 };
 
@@ -208,37 +260,37 @@ Route.prototype.all = function all() {
   //The line var handles = flatten(slice.call(arguments)); is used to gather all the middleware functions
   //passed as arguments to the all method into a single array. This allows the all method to handle multiple middleware
   //functions provided in different ways: either as separate arguments or as an array.
-/**
- * const express = require('express');
-const app = express();
-
-const middleware1 = (req, res, next) => {
-  console.log('Middleware 1');
-  next();
-};
-
-const middleware2 = (req, res, next) => {
-  console.log('Middleware 2');
-  next();
-};
-
-const middleware3 = (req, res, next) => {
-  console.log('Middleware 3');
-  next();
-};
-
-// Using the all method with separate arguments
-app.all('/path1', middleware1, middleware2);
-
-// Using the all method with an array of middleware functions
-app.all('/path2', [middleware2, middleware3]);
-
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
-
-For the /path1 route, middleware1 and middleware2 are passed as separate arguments to app.all
- */
+  /**
+   * const express = require('express');
+  const app = express();
+  
+  const middleware1 = (req, res, next) => {
+    console.log('Middleware 1');
+    next();
+  };
+  
+  const middleware2 = (req, res, next) => {
+    console.log('Middleware 2');
+    next();
+  };
+  
+  const middleware3 = (req, res, next) => {
+    console.log('Middleware 3');
+    next();
+  };
+  
+  // Using the all method with separate arguments
+  app.all('/path1', middleware1, middleware2);
+  
+  // Using the all method with an array of middleware functions
+  app.all('/path2', [middleware2, middleware3]);
+  
+  app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+  });
+  
+  For the /path1 route, middleware1 and middleware2 are passed as separate arguments to app.all
+   */
   for (var i = 0; i < handles.length; i++) {
     var handle = handles[i];
 
@@ -262,8 +314,32 @@ For the /path1 route, middleware1 and middleware2 are passed as separate argumen
 
 
 
-methods.forEach(function(method){
-  Route.prototype[method] = function(){
+
+//this piece of code handles dynamic route,let me give an example
+/**
+ * const myRoute = express.Route('/api');
+
+// Define a callback function for GET requests
+function handleGetRequest(req, res) {
+  res.send('Handling GET request on /api');
+}
+
+// Define a callback function for POST requests
+function handlePostRequest(req, res) {
+  res.send('Handling POST request on /api');
+}
+
+// Use dynamic route methods to add handlers
+myRoute.get(handleGetRequest);
+myRoute.post(handlePostRequest);
+
+// Add the route to the app
+app.use(myRoute);
+ */
+
+
+methods.forEach(function (method) {
+  Route.prototype[method] = function () {
     var handles = flatten(slice.call(arguments));
 
     for (var i = 0; i < handles.length; i++) {
